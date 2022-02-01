daf06cc5e06854fa39494acb96ba729d457ebffa
konmue <mail@konrad-mueller.com>
2022-02-01 17:13:08 +0100

    add ipynb utils

--- torch/file_per_batch_dataset.py
+++ torch/file_per_batch_dataset.py
@@ -7,7 +7,7 @@ class FilePerBatchData(Dataset):
     def __init__(self, data_dir: str, n_batches: int) -> None:
         super().__init__()
 
-        self.folder_path = data_dir
+        self.data_dir  = data_dir
         self.n_batches = n_batches
 
     def __len__(self):
